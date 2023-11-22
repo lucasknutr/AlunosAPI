dcfa43ef94ce43a9ff60c18a7ad9e27a39f7935f
Lucas <lucasclaudinocc@gmail.com>
2023-11-22 10:25:59 -0300

    Added base files

---
+++ AlunosAPI/Models/Aluno.cs
@@ -0,0 +1,9 @@
+namespace AlunosAPI.Models;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
